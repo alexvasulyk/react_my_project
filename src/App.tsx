91c925b306569bea28a89721d720fa926b514044
alexvasulyk <alexandr.vasulyk@gmail.com>
2021-10-27 20:33:52 +0300

    the_end

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss'
+import Header from './components/Header'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Profile from './components/MyProfile';
+import Slider from './components/Slider'
+import Main from './components/Main'
+import ProfileBar from './components/profileBar'
+import Cross from './images/cross.svg'
+import Footer from './components/Footer'
+import Profile_Editor from './components/MyProfile/Profile_Editor'
+import Post from './components/Main/Post/Post'
+import Login from './components/Login/index'
+import Registration from './components/Login/Registration/Registration'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Switch>
+
+          <Route exact path="/">
+            <div className="App">
+              <div className="content">
+                <div className="content-main">
+                  <Slider />
+                  <div className="new-post">New posts available
+                    <img src={Cross} alt="" />
+                  </div>
+                  <Main like={439} comment={34} />
+                  <Main like={232} comment={39} />
+                </div>
+                <div className="profile-bar">
+                  <ProfileBar />
+                  <Footer />
+                </div>
+              </div>
+            </div>
+          </Route>
+          <Route exact path="/Profile" component={Profile} />
+          <Route exact path="/profile_editor" component={Profile_Editor} />
+          <Route exact path="/post" component={Post} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/registration" component={Registration} />
+
+        </Switch>
+
+      </Router>
+    )
+  }
 }
 
-export default App;
